5c8ee7e80edef141cd54d0454b6aae735208956f
Benjamin Rodriguez <b.esteban.r.v@gmail.com>
2023-07-26 21:40:52 -0400

    Product counter creation

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,54 +1,57 @@
 import { NavLink } from "react-router-dom"
-
+import { useContext } from "react"
+import { ShoppingCartContext } from "../../Context"
 
 const Navbar = () => {
+    const context = useContext(ShoppingCartContext)
     const activeStyle = 'underline underline-offset-10'
+    
     return (
         <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light top-0">
             <ul className="flex items-center gap-3">
                 <li className="font-semibold text-lg">
-                    <NavLink 
+                    <NavLink
                         to="/">
                         Shopi
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink 
+                    <NavLink
                         to="/all"
                         className={({ isActive }) => (isActive ? activeStyle : undefined)}>
                         All
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink 
+                    <NavLink
                         to="/clothes"
                         className={({ isActive }) => (isActive ? activeStyle : undefined)}>
                         Clothes
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink 
+                    <NavLink
                         to="/electronics"
                         className={({ isActive }) => (isActive ? activeStyle : undefined)}>
                         Electronics
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink 
+                    <NavLink
                         to="/furnitures"
                         className={({ isActive }) => (isActive ? activeStyle : undefined)}>
                         Furnitures
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink 
+                    <NavLink
                         to="/toys"
                         className={({ isActive }) => (isActive ? activeStyle : undefined)}>
                         Toys
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink 
+                    <NavLink
                         to="/others"
                         className={({ isActive }) => (isActive ? activeStyle : undefined)}>
                         Others
@@ -61,37 +64,35 @@ const Navbar = () => {
                     correo.gmail.com
                 </li>
                 <li>
-                    <NavLink 
+                    <NavLink
                         to="/my-orders"
                         className={({ isActive }) => (isActive ? activeStyle : undefined)}>
                         My orders
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink 
+                    <NavLink
                         to="/my-order"
                         className={({ isActive }) => (isActive ? activeStyle : undefined)}>
                         My order
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink 
+                    <NavLink
                         to="/my-account"
                         className={({ isActive }) => (isActive ? activeStyle : undefined)}>
                         My account
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink 
+                    <NavLink
                         to="/sign-in"
                         className={({ isActive }) => (isActive ? activeStyle : undefined)}>
                         SignIn
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/">
-                        🛒
-                    </NavLink>
+                    🛒 {context.count}
                 </li>
 
             </ul>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,30 @@
+import { useState, useEffect } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 
 function Home() {
+    const [items, setItems] = useState(null)
+
+    useEffect(() => {
+        //Resepción de API con los productos
+        fetch('https://api.escuelajs.co/api/v1/products')
+            //Transformar la respuesta en JSON
+            .then(response => response.json())
+            .then(data => setItems(data))
+    }, [])
+
     return (
         <>
             <Layout>
                 Home
-                <Card />
+                <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+                    {
+                        items?.map((item) => (
+                            <Card key={item.id} data={item} />
+                        ))
+                    }
+                </div>
+
             </Layout>
         </>
     )
